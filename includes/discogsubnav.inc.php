cf259ffc2207745d5818ea34cbb8ba0caca2a26b
rfrace@nfusion.com <rfrace@nfusion.com>
2016-03-08 11:57:04 -0600

    more files

---
+++ includes/discogsubnav.inc.php
@@ -0,0 +1,13 @@
+<div id="discog_topnav" role="navigation" aria-label="Secondary">
+<h3 class="wai">Discography Sub Navigation</h3>
+<ul class="subnav">
+	<li><a href="storm.php" title="Before and After the Storm"><img src="albums/baatsmini.jpg" alt="Before and After the Storm" /></a></li>
+	<li><a href="xv.php" title="XV"><img src="albums/xvmini.jpg" alt="XV" /></a></li>
+	<li><a href="shadowpuppets.php" title="Shadow Puppets"><img src="albums/spmini.jpg" alt="Shadow Puppets" /></a></li>
+	<li><a href="asphodel.php" title="Asphodel"><img src="albums/asphodelmini.jpg" alt="Asphodel" /></a></li>
+	<li><a href="mists.php" title="Out of the Mists"><img src="albums/mistsmini.jpg" alt="Out of the Mists" /></a></li>
+	<li><a href="winters.php" title="One Winter's Night..."><img src="albums/wintersmini.jpg" alt="One Winter's Night" /></a></li>
+	<li><a href="underworld.php" title="Underworld"><img src="albums/underworldmini.jpg" alt="Underworld" /></a></li>
+	<li><a href="vs.php" title="Veils and Shadows EP"><img src="albums/vsmini.jpg" alt="Veils and Shadows" /></a></li>
+</ul>
+</div>

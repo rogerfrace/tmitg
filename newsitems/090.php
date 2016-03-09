d7d6a862c326b1df2eed00377b92564c61bdd974
rfrace@nfusion.com <rfrace@nfusion.com>
2016-03-09 09:41:32 -0600

    back in sync with prod

---
+++ newsitems/090.php
@@ -0,0 +1,7 @@
+<?php
+
+$pubdate = "Tue, 1 Dec 2015 11:10:00 CST";
+$title = "December Sale";
+$description = "15% off <a href=\"https://tmitg.bandcamp.com\" target=\"_blank\">our bandcamp store</a> all month with code &ldquo;dec2015&rdquo;. Have a great December everyone!";
+
+?>

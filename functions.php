a341125182b0b0b95d1dd6342f76b8ac9e37da8b
Roger Fracé <rogerfrace@gmail.com>
2016-11-29 18:21:50 -0600

    track num
    tweaking track number read for VO

--- functions.php
+++ functions.php
@@ -77,7 +77,7 @@ function do_mp3bc2($samplename,$songtitle=NULL,$ico=NULL) {
 // new table output structure for rows in discogs
 function do_songtitle($tracknum,$songname,$lyricsfile,$mp3name,$mp3cover,$videoname,$videotitle=NULL) {
 	echo "<tr role=\"row\" itemprop=\"track\" itemscope itemtype=\"http://schema.org/MusicRecording\">
-	<th scope=\"row\" role=\"rowheader\"><abbr title=\"track $tracknum\"><span itemprop=\"position\">$tracknum</span></abbr></th>
+	<th scope=\"row\" role=\"rowheader\"><span itemprop=\"position\" aria-label=\"track $tracknum\">$tracknum</span></th>
 	<td role=\"gridcell\" itemprop=\"name\">".stripslashes($songname)."</td>
 	<td role=\"gridcell\">";
 	

ae0388ec1a520d37e12a5872b7c8f1331a0bbb98
Roger Fracé <rogerfrace@gmail.com>
2020-04-14 13:47:01 -0500

    Shadow Puppets anniversary

---
+++ newsitems/093.php
@@ -0,0 +1,7 @@
+<?php
+
+$pubdate = "Tue, 14 Apr 2020 13:30:00 CDT";
+$title = "15th anniversary: Shadow Puppets";
+$description = "<a href=\"https://tmitg.bandcamp.com/album/shadow-puppets\"><img itemprop=\"image\" align=\"left\" hspace=\"8\" vspace=\"8\" border=\"0\" src=\"http://tmitg.com/albums/sp.jpg\" alt=\"Shadow Puppets\" /></a><p><em>Shadow Puppets</em> was released 15 years ago this month, now on sale for a limited time on our <a href=\"https://tmitg.bandcamp.com/album/shadow-puppets\">Bandcamp page</a>.</p>";
+
+?>

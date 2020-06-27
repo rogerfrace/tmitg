856026c4528e9c6f61a7a01d3605710dd09c1842
Roger Fracé <rogerfrace@gmail.com>
2020-06-27 17:03:08 -0500

    favicons

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ require_once("functions.php");
 <!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#"> <!--<![endif]-->
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="description" content="the Machine in the Garden is one of the premiere ethereal-electronic-darkwave-neoclassical-goth band in the American music industry. With 8 official releases and numerous other appearances since their founding in 1992, tMitG is one of the most lasting and prolific dark underground bands of the new era." />
+	<meta name="description" content="the Machine in the Garden is one of the premiere ethereal-electronic-darkwave-neoclassical-goth band in the American music industry. With 9 official releases and numerous other appearances since their founding in 1992, tMitG is one of the most lasting and prolific dark underground bands of the new era." />
 	<meta name="keywords" content="machine in the garden, tmitg, ethereal, electronic, darkwave, goth, gothic, old school goth, neoclassical, band, music, CDs, roger frace, summer bowman" />
 	<meta property="og:site_name" content="the Machine in the Garden" />
 	<meta property="og:title" content="the Machine in the Garden" />
 	<meta property="og:type" content="musician" />
 	<meta property="og:url" content="http://www.tmitg.com/" />
-	<meta property="og:description" content="the Machine in the Garden is one of the premiere ethereal-electronic-darkwave-neoclassical-goth band in the American music industry. With 8 official releases and numerous other appearances since their founding in 1992, tMitG is one of the most lasting and prolific dark underground bands of the new era." />
+	<meta property="og:description" content="the Machine in the Garden is one of the premiere ethereal-electronic-darkwave-neoclassical-goth band in the American music industry. With 9 official releases and numerous other appearances since their founding in 1992, tMitG is one of the most lasting and prolific dark underground bands of the new era." />
 	<meta property="og:image" content="http://www.tmitg.com/photos/xv1.jpg" />
     <meta property="og:locale" content="en_US" />
     <meta property="fb:app_id" content="121619441387">
@@ -28,12 +28,23 @@ require_once("functions.php");
 	<meta name="copyright" content="<?=date('Y',time());?>" />
 	<meta content="minimum-scale=1.0, initial-scale=1" name="viewport" />
 
-	<link rel="apple-touch-icon" href="touch-icon-precomposed.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="touch-icon-144x144-precomposed.png">
-	<meta name="msapplication-TileColor" content="#bf1025"/>
-	<meta name="msapplication-TileImage" content="touch-icon-144x144-precomposed.png">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+	<meta name="msapplication-square70x70logo" content="/ms-smalltile.png" />
+	<meta name="msapplication-square150x150logo" content="/ms-mediumtile.png" />
+	<meta name="msapplication-square310x310logo" content="/ms-largetile.png" />
 
 	<title>the Machine in the Garden</title>
 	<!--[if lt IE 9]>

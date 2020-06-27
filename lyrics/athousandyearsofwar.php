d96edf67ae087f98cc9b758cf8e23e3fb794e74b
Roger Fracé <rogerfrace@gmail.com>
2020-06-27 15:20:58 -0500

    PiB lyrics

---
+++ lyrics/athousandyearsofwar.php
@@ -0,0 +1,38 @@
+<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="no-js ie6 ie678" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7 ie678" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8 ie678" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>A Thousand Years of War</title>
+	<meta name="description" content="Lyrics for the song &quot;A Thousand Years of War&quot; by the Machine in the Garden" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="../tmitg.css" />
+	<?php include_once("../googletracking.html"); ?>
+</head>
+<body id="lyrics">
+
+<h1>A Thousand Years of War</h1>
+
+<p>(Churning the sea of milk…)</p>
+
+<p>Our precious things were lost<br>
+Jointly we search<br>
+Gods and demons<br>
+(Don't fall away)<br>
+A thousand years of war<br>
+(Don't fall away)</p>
+
+<p>In light I see<br>
+(Don't fall away)<br>
+In dark I feel<br>
+(Don't fall away)<br>
+Even now<br>
+(Don't fall away)<br>
+Even now<br>
+(Don't fall away)</p>
+      
+</body>
+</html>
---
+++ lyrics/propheticbird.php
@@ -0,0 +1,59 @@
+<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="no-js ie6 ie678" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7 ie678" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8 ie678" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Prophetic Bird</title>
+	<meta name="description" content="Lyrics for the song &quot;Prophetic Bird&quot; by the Machine in the Garden" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="../tmitg.css" />
+	<?php include_once("../googletracking.html"); ?>
+</head>
+<body id="lyrics">
+
+<h1>Prophetic Bird</h1>
+
+<p>Let me tell you of a time in the distant past<br>
+Of a girl and a bird with knowledge vast<br>
+She who knows all of beast and man<br>
+Of gods and heroes throughout the land</p>
+
+<p>Islands far, far out to sea<br>
+All alone within your tree<br>
+The secret fate of all the world<br>
+Ahead, foretold and unfurled</p>
+
+<p>Gamayun, won't you sing your tune?<br>
+Beautiful, prophetic bird</p>
+
+<p>Let me tell you of a time in the distant past<br>
+A deity of wisdom, the die was cast<br>
+Singing herself a divine melody<br>
+A voice throaty and velvety</p>
+
+<p>Her song weaves a web of magic and fate<br>
+Hard to interpret until it's too late<br>
+Who among us can comprehend?<br>
+Not I, for certain, dear friend</p>
+
+<p>Gamayun, won't you sing your tune?<br>
+Beautiful, prophetic bird</p>
+
+<p>Let me tell you of a time in the distant past<br>
+Of elements and nature, the world vast<br>
+Sun, rain, wind, animals and birds<br>
+Plants and trees, they hear her words</p>
+
+<p>Ancient origins and time now lost<br>
+Opulence and decadence at great cost<br>
+Her song now gone and dreams now hushed<br>
+Wings and feathers, in the ground, crushed</p>
+
+<p>Gamayun, won't you sing your tune?<br>
+Beautiful, prophetic bird</p>
+      
+</body>
+</html>

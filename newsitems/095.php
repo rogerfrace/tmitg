7e351dfc23dfb0fbff6f5837a14206c2e36b1064
Roger Fracé <rogerfrace@gmail.com>
2020-07-07 10:32:49 -0500

    PiB release newsitem

---
+++ newsitems/095.php
@@ -0,0 +1,19 @@
+<?php
+
+$pubdate = "Fri, 10 Jul 2020 00:00:00 CDT";
+$title = "Now Available: <em>Places in Between</em>";
+$description = "<a href=\"https://tmitg.bandcamp.com/album/places-in-between\"><img itemprop=\"image\" align=\"left\" hspace=\"8\" vspace=\"8\" border=\"0\" src=\"http://tmitg.com/albums/places.jpg\" alt=\"Places in Between album on Bandcamp\" /></a>";
+
+$description .= "<p><strong>the Machine in the Garden's</strong> ninth studio album and our first in nine years, <i>Places in Between</i>, is now available for digital download on <a href=\"https://tmitg.bandcamp.com/album/places-in-between\">our Bandcamp page</a>. We are also accepting pre-orders for <a href=\"https://tmitg.bandcamp.com/merch\">the physical CD and various combos</a> including a CD with a t-shirt and/or a handwritten lyrics sheet.</p>";
+
+$description .= "<p>Thirteen new songs explore the variety of styles&mdash;from goth rock to electronic ethereal&mdash;the band is known for.</p>";
+
+$description .= "<p>Musically, <i>Places in Between</i> draws from the band's own unique musical past. Roger describes the sound as &quot;the electronics and looping structure of <i>Shadow Puppets</i>, the darkness of <i>Asphodel</i>, and epic elements of <i>Before and After the Storm</i>. I wasn't trying to emulate anyone or anything else; I just constructed what felt natural. It's different and should feel new yet familiar to our fans.&quot;</p>";
+
+$description .= "<p>Like previous works, the themes explored by the lyrics are wide-ranging and include references from mythology of world cultures (like in &quot;A Thousand Years of War&quot; and &quot;Prophetic Bird&quot;), themes from popular culture (&quot;Courage, Power, and Wisdom&quot;) as well as references to current events and challenges we face in the world (&quot;Broken Days&quot; and &quot;Resist&quot;). No matter the topic, Bowman's lyrics and vocal delivery are full of emotion and depth and her voice shines through clear and full in every track.</p>";
+
+$description .= "<p><em>For the audio nerds:</em> 24-bit files were uploaded exclusively to Bandcamp, so for the best quality choose a lossless file format for download.</p>";
+
+$description .= "<p><a href=\"https://youtu.be/o1ai_xl0Lcw\">Watch the <em>Places in Between</em> teaser video</a></p>";
+
+?>

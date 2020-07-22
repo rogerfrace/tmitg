5357a8a4850e1792dab1981ed01fec57b9c78584
Roger Fracé <rogerfrace@gmail.com>
2020-07-22 10:29:31 -0500

    titles on compilation links

--- discog.php
+++ discog.php
@@ -60,61 +60,61 @@
 <section aria-labelledby="compilations">
 <h2 id="compilations">compilation appearances:</h2>
 <ul class="comps">
-<li><a href="rebellion.php"><img src="albums/rebellionico.png" alt="Rebellion: Vol 1"></a></li>
+<li><a href="rebellion.php" title="Rebellion: Vol 1"><img src="albums/rebellionico.png" alt="Rebellion: Vol 1"></a></li>
 
-<li><a href="drownedvisions.php"><img src="albums/drownedvisionsico.jpg" alt="Drowned Visions"></a></li>
+<li><a href="drownedvisions.php" title="Drowned Visions"><img src="albums/drownedvisionsico.jpg" alt="Drowned Visions"></a></li>
 
-<li><a href="attrition.php"><img src="albums/attritionico.jpg" alt="Wrapped in the Guise of My Friend"></a></li>
+<li><a href="attrition.php" title="Wrapped in the Guise of My Friend"><img src="albums/attritionico.jpg" alt="Wrapped in the Guise of My Friend"></a></li>
 
-<li><a href="unquietultimate.php"><img src="albums/uqguico.jpg" alt="The Unquiet Grave - The Ultimate Goth Collection"></a></li>
+<li><a href="unquietultimate.php" title="The Unquiet Grave - The Ultimate Goth Collection"><img src="albums/uqguico.jpg" alt="The Unquiet Grave - The Ultimate Goth Collection"></a></li>
 
-<li><a href="darkhorizons.php"><img src="albums/darkhorizonsico.jpg" alt="Dark Horizons: Dance of the Dead"></a></li>
+<li><a href="darkhorizons.php" title="Dark Horizons: Dance of the Dead"><img src="albums/darkhorizonsico.jpg" alt="Dark Horizons: Dance of the Dead"></a></li>
 
-<li><a href="c11.php"><img src="albums/c11ico.jpg" alt="Convergence 11 CD Sampler"></a></li>
+<li><a href="c11.php" title="Convergence 11 CD Sampler"><img src="albums/c11ico.jpg" alt="Convergence 11 CD Sampler"></a></li>
 
-<li><a href="eclectica2.php"><img src="albums/eclectica2ico.jpg" alt="Eclectica 2"></a></li>
+<li><a href="eclectica2.php" title="Eclectica 2"><img src="albums/eclectica2ico.jpg" alt="Eclectica 2"></a></li>
 
-<li><a href="bets.php"><img src="albums/betsico.jpg" alt="Cover Your Bets!"></a></li>
+<li><a href="bets.php" title="Cover Your Bets!"><img src="albums/betsico.jpg" alt="Cover Your Bets!"></a></li>
 
-<li><a href="elegy.php"><img src="albums/elegyico.jpg" alt="Elegy Sampler 25"></a></li>
+<li><a href="elegy.php" title="Elegy Sampler 25"><img src="albums/elegyico.jpg" alt="Elegy Sampler 25"></a></li>
 
-<li><a href="eclectica.php"><img src="albums/eclecticaico.jpg" alt="Eclectica!"></a></li>
+<li><a href="eclectica.php" title="Eclectica!"><img src="albums/eclecticaico.jpg" alt="Eclectica!"></a></li>
 
-<li><a href="misfit.php"><img src="albums/misfitico.jpg" alt="A Misfit Xmas"></a></li>
+<li><a href="misfit.php" title="A Misfit Xmas"><img src="albums/misfitico.jpg" alt="A Misfit Xmas"></a></li>
 
-<li><a href="PoW.php"><img src="albums/PoWico.jpg" alt="Palace of Worms"></a></li>
+<li><a href="PoW.php" title="Palace of Worms"><img src="albums/PoWico.jpg" alt="Palace of Worms"></a></li>
 
-<li><a href="cantus.php"><img src="albums/cantusico.jpg" alt="Cantus"></a></li>
+<li><a href="cantus.php" title="Cantus"><img src="albums/cantusico.jpg" alt="Cantus"></a></li>
 
-<li><a href="tnog.php"><img src="albums/tnogico.jpg" alt="The Whitby Compilation"></a></li>
+<li><a href="tnog.php" title="The Whitby Compilation"><img src="albums/tnogico.jpg" alt="The Whitby Compilation"></a></li>
 
-<li><a href="sde.php"><img src="albums/sdeico.jpg" alt="Sex Death and Eyeliner"></a></li>
+<li><a href="sde.php" title="Sex Death and Eyeliner"><img src="albums/sdeico.jpg" alt="Sex Death and Eyeliner"></a></li>
 
-<li><a href="tvccharity2.php"><img src="albums/tvccharity2ico.jpg" alt="The Violet Collection 2"></a></li>
+<li><a href="tvccharity2.php" title="The Violet Collection 2"><img src="albums/tvccharity2ico.jpg" alt="The Violet Collection 2"></a></li>
 
-<li><a href="butoh.php"><img src="albums/butohico.jpg" alt="Butoh"></a></li>
+<li><a href="butoh.php" title="Butoh"><img src="albums/butohico.jpg" alt="Butoh"></a></li>
 
-<li><a href="legends.php"><img src="albums/legendsico.jpg" alt="Immortal Legends"></a></li>
+<li><a href="legends.php" title="Immortal Legends"><img src="albums/legendsico.jpg" alt="Immortal Legends"></a></li>
 
-<li><a href="cocteau.php"><img src="albums/cocteauico.jpg" alt="Cocteau Twins Tribute"></a></li>
+<li><a href="cocteau.php" title="Dark Treasures"><img src="albums/cocteauico.jpg" alt="Dark Treasures"></a></li>
 
-<li><a href="tvccharity.php"><img src="albums/tvccharityico.jpg" alt="The Violet Collection"></a></li>
+<li><a href="tvccharity.php" title="The Violet Collection"><img src="albums/tvccharityico.jpg" alt="The Violet Collection"></a></li>
 
-<li><a href="resistor.php"><img src="albums/resistorico.jpg" alt="Resistor"></a></li>
+<li><a href="resistor.php" title="Resistor"><img src="albums/resistorico.jpg" alt="Resistor"></a></li>
 
-<li><a href="excelsis.php"><img src="albums/excelsisico.jpg" alt="Excelsis Vol. 2"></a></li>
+<li><a href="excelsis.php" title="Excelsis Vol. 2"><img src="albums/excelsisico.jpg" alt="Excelsis Vol. 2"></a></li>
 
-<li><a href="unquiet.php"><img src="albums/uqgico.jpg" alt="The Unquiet Grave"></a></li>
+<li><a href="unquiet.php" title="The Unquiet Grave"><img src="albums/uqgico.jpg" alt="The Unquiet Grave"></a></li>
 
-<li><a href="eternal.php"><img src="albums/eternalico.jpg" alt="What Is Eternal"></a></li>
+<li><a href="eternal.php" title="What Is Eternal"><img src="albums/eternalico.jpg" alt="What Is Eternal"></a></li>
 
-<li><a href="arofv3.php"><img src="albums/arofv3ico.jpg" alt="Abort Retry or Fail Vol. 3"></a></li>
+<li><a href="arofv3.php" title="Abort Retry or Fail Vol. 3"><img src="albums/arofv3ico.jpg" alt="Abort Retry or Fail Vol. 3"></a></li>
 
-<li><a href="sogrey.php"><img src="albums/sogreyico.jpg" alt="Shades of Grey"></a></li>
+<li><a href="sogrey.php" title="Shades of Grey"><img src="albums/sogreyico.jpg" alt="Shades of Grey"></a></li>
 
-<li><a href="hexfiles.php"><img src="albums/hexfilesico.jpg" alt="Hex Files, Vol. 2"></a></li>
+<li><a href="hexfiles.php" title="Hex Files, Vol. 2"><img src="albums/hexfilesico.jpg" alt="Hex Files, Vol. 2"></a></li>
 
-<li><a href="mindbody.php"><img src="albums/mindbodyico.jpg" alt="Mind/Body2"></a></li>
+<li><a href="mindbody.php" title="Mind/Body2"><img src="albums/mindbodyico.jpg" alt="Mind/Body2"></a></li>
 </ul>
 </section>
 

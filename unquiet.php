54acb014911e2b283377d842de962c577e7933cd
Roger Fracé <rogerfrace@gmail.com>
2022-04-29 09:28:04 -0500

    Vol 1 to Volume One

--- unquiet.php
+++ unquiet.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<title>the Machine in the Garden - The Unquiet Grave Vol. 1</title>
+	<title>the Machine in the Garden - The Unquiet Grave Volume One</title>
 	<meta name="copyright" content="<?=date('Y',time());?>">
 	<?php include_once("headers-additional.php"); ?>
 </head>
@@ -26,7 +26,7 @@
 <!-- this is the display table for the CD and info -->
 <div id="discog_albuminfo" tabindex="-1">
 	<img src="albums/uqg.jpg" alt="The Unquiet Grave" width="250" height="250">
-	<h1>The Unquiet Grave Vol. 1</h1>
+	<h1>The Unquiet Grave Volume One</h1>
 	<p class="notopmargin">(CLP0524) Cleopatra Records (USA) &copy;1999<br>
 	(LM 103) Locomotive Music (Spain) &copy;1999</p>
 </div> <!-- end album info div -->

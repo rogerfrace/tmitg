4b10120c196c548e38c00a3a62068a29ebbdd870
Roger Fracé <rogerfrace@gmail.com>
2022-05-04 09:50:39 -0500

    async jquery bad. removing.

--- headers-additional.php
+++ headers-additional.php
@@ -3,7 +3,7 @@
 <meta property="fb:admins" content="1120445561,713632115">
 <link rel="preload" as="style" href="tmitg-mobile.css">
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" async></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 <?php if (check_mobile()==false): ?>
 <!-- colorbox --> <!-- async load from https://web.dev/defer-non-critical-css/ -->

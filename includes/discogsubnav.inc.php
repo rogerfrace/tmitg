6c8346afc56073df92660e9535d6bb5a82a5b4c5
Roger Fracé <rogerfrace@gmail.com>
2022-05-06 14:03:15 -0500

    2x image discog subnav

--- includes/discogsubnav.inc.php
+++ includes/discogsubnav.inc.php
@@ -1,13 +1,13 @@
 <div id="discog_topnav" role="navigation" aria-label="Secondary" tabindex="-1">
 <ul class="subnav">
-	<li><a href="places.php" title="Places in Between"><img src="albums/placesmini.jpg" alt="Places in Between" width="46" height="46" /></a></li>
-	<li><a href="storm.php" title="Before and After the Storm"><img src="albums/baatsmini.jpg" alt="Before and After the Storm" width="46" height="46" /></a></li>
-	<li><a href="xv.php" title="XV"><img src="albums/xvmini.jpg" alt="XV" width="46" height="46" /></a></li>
-	<li><a href="shadowpuppets.php" title="Shadow Puppets"><img src="albums/spmini.jpg" alt="Shadow Puppets" width="46" height="46" /></a></li>
-	<li><a href="asphodel.php" title="Asphodel"><img src="albums/asphodelmini.jpg" alt="Asphodel" width="46" height="46" /></a></li>
-	<li><a href="mists.php" title="Out of the Mists"><img src="albums/mistsmini.jpg" alt="Out of the Mists" width="46" height="46" /></a></li>
-	<li><a href="winters.php" title="One Winter's Night..."><img src="albums/wintersmini.jpg" alt="One Winter's Night" width="46" height="46" /></a></li>
-	<li><a href="underworld.php" title="Underworld"><img src="albums/underworldmini.jpg" alt="Underworld" width="46" height="46" /></a></li>
-	<li><a href="vs.php" title="Veils and Shadows EP"><img src="albums/vsmini.jpg" alt="Veils and Shadows" width="46" height="46" /></a></li>
+	<li><a href="places.php" title="Places in Between"><picture><source srcset="albums/placesmini92.jpg 2x"><img src="albums/placesmini.jpg" alt="Places in Between" width="46" height="46" /></picture></a></li>
+	<li><a href="storm.php" title="Before and After the Storm"><picture><source srcset="albums/baatsmini92.jpg 2x"><img src="albums/baatsmini.jpg" alt="Before and After the Storm" width="46" height="46" /></a></li>
+	<li><a href="xv.php" title="XV"><picture><source srcset="albums/xvmini92.jpg 2x"><img src="albums/xvmini.jpg" alt="XV" width="46" height="46" /></picture></a></li>
+	<li><a href="shadowpuppets.php" title="Shadow Puppets"><picture><source srcset="albums/spmini92.jpg 2x"><img src="albums/spmini.jpg" alt="Shadow Puppets" width="46" height="46" /></picture></a></li>
+	<li><a href="asphodel.php" title="Asphodel"><picture><source srcset="albums/asphodelmini92.jpg 2x"><img src="albums/asphodelmini.jpg" alt="Asphodel" width="46" height="46" /></picture></a></li>
+	<li><a href="mists.php" title="Out of the Mists"><picture><source srcset="albums/mistsmini92.jpg 2x"><img src="albums/mistsmini.jpg" alt="Out of the Mists" width="46" height="46" /></picture></a></li>
+	<li><a href="winters.php" title="One Winter's Night..."><picture><source srcset="albums/wintersmini92.jpg 2x"><img src="albums/wintersmini.jpg" alt="One Winter's Night" width="46" height="46" /></picture></a></li>
+	<li><a href="underworld.php" title="Underworld"><picture><source srcset="albums/underworldmini92.jpg 2x"><img src="albums/underworldmini.jpg" alt="Underworld" width="46" height="46" /></picture></a></li>
+	<li><a href="vs.php" title="Veils and Shadows EP"><picture><source srcset="albums/vsmini92.jpg 2x"><img src="albums/vsmini.jpg" alt="Veils and Shadows" width="46" height="46" /></picture></a></li>
 </ul>
 </div>

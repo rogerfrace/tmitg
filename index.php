2e66fff41b8f6334141e55ad0aec0187fa7a81b0
Roger Fracé <rogerfrace@gmail.com>
2022-05-07 11:03:59 -0500

    removing all godaddy php gzip code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start();
+<?php
 $top = !empty($_POST['top']) ? $_POST['top'] : '';
 if ($top=="merch.html") { header("Location:merch.php"); }
 require_once("functions.php");
--- viewmp3bc.php
+++ viewmp3bc.php
@@ -1,4 +1,3 @@
-<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
 <?php require_once("functions.php"); ?>
 <?php
 if (!empty($_GET['bcsample'])){

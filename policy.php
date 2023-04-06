7cd763271f540f8a869b46b4771fa8ee4b017615
Roger Fracé <rogerfrace@gmail.com>
2023-04-06 15:58:58 -0500

    tc+policy update

--- policy.php
+++ policy.php
@@ -16,15 +16,15 @@
 
 <h1>Policies</h1>
 
-<small>Last updated <time>2021-05-07</time></small>
+<small>Last updated <time>2023-04-06</time></small>
 
 <h2>Return Policy</h2>
 <p>Unopened CDs may be returned within 30 days for a full refund less shipping. Shipping charges are not refundable. Opened CDs may only be returned in the event of a defective CD. If your CD is defective, we will pay return shipping and we will pay to ship a new CD to you.</p>
 
 <h2>Shipping Policy</h2>
 <p>We generally ship orders within 5 business days.</p>
-<p>Shipping prices for a single CD in the United States (As of March 2021):
-<br>First Class Mail - $4.45</p>
+<p>Shipping prices for a single CD in the United States (As of January 2023):
+<br>First Class Mail - $5.60</p>
 
 <h2>Privacy Policy</h2>
 
@@ -36,9 +36,6 @@ Tracking cookies, including but not limited to Google Analytics, are used for tr
 
 <h3>Contact Information</h3>
 <p>No contact information for visitors to this website will ever be given to a third party. Physical and electronic contact information provided on this site will only be used by the Machine in the Garden and related projects and properties, including but not limited to Deus ex Musica and Mirabilis.</p>
-
-<h3>Data Transmission</h3>
-<p>This website does not use any web secure protocols so all information transmitted is insecure.</p>
 
 <h2>Terms of Service</h2>
 <p>No guarantees are made regarding quality, compatibility, functionality, accessibility, or content of this website &mdash; we simply do our best to be as cross-compatible, accessible and user-friendly as possible. If you discover any problems with out website, please <a href="contact.php">contact us</a>.</p>

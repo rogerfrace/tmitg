1a726e7ee0c338a527b0fea4b5d6810e0fa36e45
Roger Fracé <rogerfrace@gmail.com>
2024-12-04 18:18:07 -0600

    width and height attributes for images

--- discog.php
+++ discog.php
@@ -26,102 +26,102 @@
 
 <ul role="list" class="releases">
 
-<li><a href="places.php" title="Places in Between"><img src="albums/placesico.jpg" alt="Places in Between"></a></li>
+<li><a href="places.php" title="Places in Between"><img src="albums/placesico.jpg" width="139" height="125" alt="Places in Between"></a></li>
 
-<li><a href="imageacoustic.php" title="Image (acoustic)"><img src="albums/imageacousticico.jpg" alt="Image (acoustic)"></a></li>
+<li><a href="imageacoustic.php" title="Image (acoustic)"><img src="albums/imageacousticico.jpg" width="125" height="125" alt="Image (acoustic)"></a></li>
 
-<li><a href="miscellany.php" title="Miscellany"><img src="albums/miscellanyico.jpg" alt="Miscellany"></a></li>
+<li><a href="miscellany.php" title="Miscellany"><img src="albums/miscellanyico.jpg" width="125" height="125" alt="Miscellany"></a></li>
 
-<li><a href="storm.php" title="Before and After the Storm"><img src="albums/baatsico.jpg" alt="Before and After the Storm"></a></li>
+<li><a href="storm.php" title="Before and After the Storm"><img src="albums/baatsico.jpg" width="124" height="125" alt="Before and After the Storm"></a></li>
 
-<li><a href="vanir.php" title="in the vanir"><img src="albums/vanirico.jpg" alt="In the Vanir"></a></li>
+<li><a href="vanir.php" title="in the vanir"><img src="albums/vanirico.jpg" width="125" height="125" alt="In the Vanir"></a></li>
 
-<li><a href="xv.php" title="XV"><img src="albums/xvico.jpg" alt="XV"></a></li>
+<li><a href="xv.php" title="XV"><img src="albums/xvico.jpg" width="125" height="125" alt="XV"></a></li>
 
-<li><a href="shadowpuppets.php" title="Shadow Puppets"><img src="albums/spico.jpg" alt="Shadow Puppets"></a></li>
+<li><a href="shadowpuppets.php" title="Shadow Puppets"><img src="albums/spico.jpg" width="125" height="125" alt="Shadow Puppets"></a></li>
 
-<li><a href="asphodel.php" title="Asphodel"><img src="albums/asphodelico.jpg" alt="Asphodel"></a></li>
+<li><a href="asphodel.php" title="Asphodel"><img src="albums/asphodelico.jpg" width="125" height="127" alt="Asphodel"></a></li>
 
-<li><a href="wapfuu2.php" title="When Angels Peer Favorably Upon Us, vol. 2"><img src="albums/wapfuu2ico.jpg" alt="When Angels Peer Favorably Upon Us, Vol. 2"></a></li>
+<li><a href="wapfuu2.php" title="When Angels Peer Favorably Upon Us, vol. 2"><img src="albums/wapfuu2ico.jpg" width="125" height="125" alt="When Angels Peer Favorably Upon Us, Vol. 2"></a></li>
 
-<li><a href="mists.php" title="Out of the Mists"><img src="albums/mistsico.jpg" alt="Out of the Mists"></a></li>
+<li><a href="mists.php" title="Out of the Mists"><img src="albums/mistsico.jpg" width="140" height="125" alt="Out of the Mists"></a></li>
 
-<li><a href="wapfuu1.php" title="When Angels Peer Favorably Upon Us, vol. 1"><img src="albums/wapfuu1ico.jpg" alt="When Angels Peer Favorably Upon Us, Vol. 1"></a></li>
+<li><a href="wapfuu1.php" title="When Angels Peer Favorably Upon Us, vol. 1"><img src="albums/wapfuu1ico.jpg" width="125" height="125" alt="When Angels Peer Favorably Upon Us, Vol. 1"></a></li>
 
-<li><a href="winters.php" title="One Winter's Night..."><img src="albums/wintersico.jpg" alt="One Winter's Night..."></a></li>
+<li><a href="winters.php" title="One Winter's Night..."><img src="albums/wintersico.jpg" width="141" height="125" alt="One Winter's Night..."></a></li>
 
-<li><a href="underworld.php" title="Underworld"><img src="albums/underworldico.jpg" alt="Underworld"></a></li>
+<li><a href="underworld.php" title="Underworld"><img src="albums/underworldico.jpg" width="125" height="125" alt="Underworld"></a></li>
 
-<li><a href="vsremix.php" title="Veils and Shadows Remixes"><img src="albums/vsremixico.jpg" alt="Veils and Shadows Remixes"></a></li>
+<li><a href="vsremix.php" title="Veils and Shadows Remixes"><img src="albums/vsremixico.jpg" width="125" height="125" alt="Veils and Shadows Remixes"></a></li>
 
-<li><a href="vs.php" title="Veils and Shadows EP"><img src="albums/vsico.jpg" alt="Veils and Shadows EP"></a></li>
+<li><a href="vs.php" title="Veils and Shadows EP"><img src="albums/vsico.jpg" width="125" height="125" alt="Veils and Shadows EP"></a></li>
 </ul>
 </section>
 
 <section aria-labelledby="compilations">
 <h2 id="compilations">compilation appearances:</h2>
 <ul role="list" class="comps">
-<li><a href="unquietfinal.php" title="The Unquiet Grave: The Final Chapter"><img src="albums/unquietfinalico.jpg" alt="The Unquiet Grave: The Final Chapter"></a></li>
+<li><a href="unquietfinal.php" title="The Unquiet Grave: The Final Chapter"><img src="albums/unquietfinalico.jpg" width="75" height="75" alt="The Unquiet Grave: The Final Chapter"></a></li>
 
-<li><a href="unearthd.php" title="Unearth'd"><img src="albums/unearthdico.jpg" alt="Unearth'd"></a></li>
+<li><a href="unearthd.php" title="Unearth'd"><img src="albums/unearthdico.jpg" width="75" height="75" alt="Unearth'd"></a></li>
 
-<li><a href="ladansemacabre9.php" title="La Danse Macabre 9"><img src="albums/ladansemacabre9ico.jpg" alt="La Danse Macabre 9"></a></li>
+<li><a href="ladansemacabre9.php" title="La Danse Macabre 9"><img src="albums/ladansemacabre9ico.jpg" width="75" height="75" alt="La Danse Macabre 9"></a></li>
 
-<li><a href="rebellion.php" title="Rebellion: Vol 1"><img src="albums/rebellionico.png" alt="Rebellion: Vol 1"></a></li>
+<li><a href="rebellion.php" title="Rebellion: Vol 1"><img src="albums/rebellionico.png" width="75" height="75" alt="Rebellion: Vol 1"></a></li>
 
-<li><a href="drownedvisions.php" title="Drowned Visions"><img src="albums/drownedvisionsico.jpg" alt="Drowned Visions"></a></li>
+<li><a href="drownedvisions.php" title="Drowned Visions"><img src="albums/drownedvisionsico.jpg" width="75" height="75" alt="Drowned Visions"></a></li>
 
-<li><a href="attrition.php" title="Wrapped in the Guise of My Friend"><img src="albums/attritionico.jpg" alt="Wrapped in the Guise of My Friend"></a></li>
+<li><a href="attrition.php" title="Wrapped in the Guise of My Friend"><img src="albums/attritionico.jpg" width="75" height="75" alt="Wrapped in the Guise of My Friend"></a></li>
 
-<li><a href="unquietultimate.php" title="The Unquiet Grave - The Ultimate Goth Collection"><img src="albums/uqguico.jpg" alt="The Unquiet Grave - The Ultimate Goth Collection"></a></li>
+<li><a href="unquietultimate.php" title="The Unquiet Grave - The Ultimate Goth Collection"><img src="albums/uqguico.jpg" width="75" height="75" alt="The Unquiet Grave - The Ultimate Goth Collection"></a></li>
 
-<li><a href="darkhorizons.php" title="Dark Horizons: Dance of the Dead"><img src="albums/darkhorizonsico.jpg" alt="Dark Horizons: Dance of the Dead"></a></li>
+<li><a href="darkhorizons.php" title="Dark Horizons: Dance of the Dead"><img src="albums/darkhorizonsico.jpg" width="75" height="75" alt="Dark Horizons: Dance of the Dead"></a></li>
 
-<li><a href="c11.php" title="Convergence 11 CD Sampler"><img src="albums/c11ico.jpg" alt="Convergence 11 CD Sampler"></a></li>
+<li><a href="c11.php" title="Convergence 11 CD Sampler"><img src="albums/c11ico.jpg" width="75" height="75" alt="Convergence 11 CD Sampler"></a></li>
 
-<li><a href="eclectica2.php" title="Eclectica 2"><img src="albums/eclectica2ico.jpg" alt="Eclectica 2"></a></li>
+<li><a href="eclectica2.php" title="Eclectica 2"><img src="albums/eclectica2ico.jpg" width="84" height="75" alt="Eclectica 2"></a></li>
 
-<li><a href="bets.php" title="Cover Your Bets!"><img src="albums/betsico.jpg" alt="Cover Your Bets!"></a></li>
+<li><a href="bets.php" title="Cover Your Bets!"><img src="albums/betsico.jpg" width="75" height="75" alt="Cover Your Bets!"></a></li>
 
-<li><a href="elegy.php" title="Elegy Sampler 25"><img src="albums/elegyico.jpg" alt="Elegy Sampler 25"></a></li>
+<li><a href="elegy.php" title="Elegy Sampler 25"><img src="albums/elegyico.jpg" width="75" height="75" alt="Elegy Sampler 25"></a></li>
 
-<li><a href="eclectica.php" title="Eclectica!"><img src="albums/eclecticaico.jpg" alt="Eclectica!"></a></li>
+<li><a href="eclectica.php" title="Eclectica!"><img src="albums/eclecticaico.jpg" width="75" height="75" alt="Eclectica!"></a></li>
 
-<li><a href="misfit.php" title="A Misfit Xmas"><img src="albums/misfitico.jpg" alt="A Misfit Xmas"></a></li>
+<li><a href="misfit.php" title="A Misfit Xmas"><img src="albums/misfitico.jpg" width="75" height="75" alt="A Misfit Xmas"></a></li>
 
-<li><a href="PoW.php" title="Palace of Worms"><img src="albums/PoWico.jpg" alt="Palace of Worms"></a></li>
+<li><a href="PoW.php" title="Palace of Worms"><img src="albums/PoWico.jpg" width="75" height="75" alt="Palace of Worms"></a></li>
 
-<li><a href="cantus.php" title="Cantus"><img src="albums/cantusico.jpg" alt="Cantus"></a></li>
+<li><a href="cantus.php" title="Cantus"><img src="albums/cantusico.jpg" width="75" height="75" alt="Cantus"></a></li>
 
-<li><a href="tnog.php" title="The Whitby Compilation"><img src="albums/tnogico.jpg" alt="The Whitby Compilation"></a></li>
+<li><a href="tnog.php" title="The Whitby Compilation"><img src="albums/tnogico.jpg" width="75" height="75" alt="The Whitby Compilation"></a></li>
 
-<li><a href="sde.php" title="Sex Death and Eyeliner"><img src="albums/sdeico.jpg" alt="Sex Death and Eyeliner"></a></li>
+<li><a href="sde.php" title="Sex Death and Eyeliner"><img src="albums/sdeico.jpg" width="76" height="75" alt="Sex Death and Eyeliner"></a></li>
 
-<li><a href="tvccharity2.php" title="The Violet Collection 2"><img src="albums/tvccharity2ico.jpg" alt="The Violet Collection 2"></a></li>
+<li><a href="tvccharity2.php" title="The Violet Collection 2"><img src="albums/tvccharity2ico.jpg" width="75" height="75" alt="The Violet Collection 2"></a></li>
 
-<li><a href="butoh.php" title="Butoh"><img src="albums/butohico.jpg" alt="Butoh"></a></li>
+<li><a href="butoh.php" title="Butoh"><img src="albums/butohico.jpg" width="83" height="75" alt="Butoh"></a></li>
 
-<li><a href="legends.php" title="Immortal Legends"><img src="albums/legendsico.jpg" alt="Immortal Legends"></a></li>
+<li><a href="legends.php" title="Immortal Legends"><img src="albums/legendsico.jpg" width="75" height="75" alt="Immortal Legends"></a></li>
 
-<li><a href="cocteau.php" title="Dark Treasures"><img src="albums/cocteauico.jpg" alt="Dark Treasures"></a></li>
+<li><a href="cocteau.php" title="Dark Treasures"><img src="albums/cocteauico.jpg" width="75" height="75" alt="Dark Treasures"></a></li>
 
-<li><a href="tvccharity.php" title="The Violet Collection"><img src="albums/tvccharityico.jpg" alt="The Violet Collection"></a></li>
+<li><a href="tvccharity.php" title="The Violet Collection"><img src="albums/tvccharityico.jpg" width="75" height="75" alt="The Violet Collection"></a></li>
 
-<li><a href="resistor.php" title="Resistor"><img src="albums/resistorico.jpg" alt="Resistor"></a></li>
+<li><a href="resistor.php" title="Resistor"><img src="albums/resistorico.jpg" width="75" height="75" alt="Resistor"></a></li>
 
-<li><a href="excelsis.php" title="Excelsis Vol. 2"><img src="albums/excelsisico.jpg" alt="Excelsis Vol. 2"></a></li>
+<li><a href="excelsis.php" title="Excelsis Vol. 2"><img src="albums/excelsisico.jpg" width="75" height="75" alt="Excelsis Vol. 2"></a></li>
 
-<li><a href="unquiet.php" title="The Unquiet Grave Volume One"><img src="albums/uqgico.jpg" alt="The Unquiet Grave Volume One"></a></li>
+<li><a href="unquiet.php" title="The Unquiet Grave Volume One"><img src="albums/uqgico.jpg" width="75" height="75" alt="The Unquiet Grave Volume One"></a></li>
 
-<li><a href="eternal.php" title="What Is Eternal"><img src="albums/eternalico.jpg" alt="What Is Eternal"></a></li>
+<li><a href="eternal.php" title="What Is Eternal"><img src="albums/eternalico.jpg" width="87" height="75" alt="What Is Eternal"></a></li>
 
-<li><a href="arofv3.php" title="Abort Retry or Fail Vol. 3"><img src="albums/arofv3ico.jpg" alt="Abort Retry or Fail Vol. 3"></a></li>
+<li><a href="arofv3.php" title="Abort Retry or Fail Vol. 3"><img src="albums/arofv3ico.jpg" width="77" height="75" alt="Abort Retry or Fail Vol. 3"></a></li>
 
-<li><a href="sogrey.php" title="Shades of Grey"><img src="albums/sogreyico.jpg" alt="Shades of Grey"></a></li>
+<li><a href="sogrey.php" title="Shades of Grey"><img src="albums/sogreyico.jpg" width="77" height="75" alt="Shades of Grey"></a></li>
 
-<li><a href="hexfiles.php" title="Hex Files, Vol. 2"><img src="albums/hexfilesico.jpg" alt="Hex Files, Vol. 2"></a></li>
+<li><a href="hexfiles.php" title="Hex Files, Vol. 2"><img src="albums/hexfilesico.jpg" width="84" height="75" alt="Hex Files, Vol. 2"></a></li>
 
-<li><a href="mindbody.php" title="Mind/Body2"><img src="albums/mindbodyico.jpg" alt="Mind/Body2"></a></li>
+<li><a href="mindbody.php" title="Mind/Body2"><img src="albums/mindbodyico.jpg" width="75" height="75" alt="Mind/Body2"></a></li>
 </ul>
 </section>
 

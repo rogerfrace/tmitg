2900c0e77eea2959106e8fb741d5505bc6dad451
Roger Fracé <rogerfrace@gmail.com>
2024-12-04 18:38:07 -0600

    lighthouse performance improvement

--- headers-additional.php
+++ headers-additional.php
@@ -1,6 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="fb:app_id" content="121619441387">
 <meta property="fb:admins" content="1120445561,713632115">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preload" as="style" href="tmitg-mobile.css">
 <?php 
 	if ($_SERVER['PHP_SELF'] != "/news.php") {

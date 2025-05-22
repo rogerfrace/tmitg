1a37d86b43b6da399997eb4e635b33bce62ec0bb
Roger Fracé <rogerfrace@gmail.com>
2025-05-22 06:45:12 -0500

    Cold 2025 news

---
+++ newsitems/101.php
@@ -0,0 +1,9 @@
+<?php
+
+$pubdate = "Thu, 22 May 2025 07:00:00 CDT";
+$title = "Cold (2025)";
+$description = "<img itemprop=\"image\" src=\"https://www.tmitg.com/photos/dgtmitg7.jpg\" alt=\"Roger and Summer at a piano\" width=\"301\" height=\"240\" /><p>Old song, new arrangement. Have a spooky #WorldGothDay everyone! <a href=\"https://soundcloud.com/the-machine-in-the-garden/cold-2025\">Listen to \"Cold (2025)\" on Soundcloud.</a></p>";
+
+?>
+
+

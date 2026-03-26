448daca7d1146f884c546f290a8609215ad8c5f1
Roger Fracé <rogerfrace@gmail.com>
2026-03-26 18:32:18 -0500

    stub Microdata schema on upcoming live show code

--- index.php
+++ index.php
@@ -190,14 +190,16 @@ require_once "functions.php";
 		<!-- live shows section -->
 		<h2>Live Shows</h2>
 
-		<!--<p class="notopmargin"><b>Saturday, November 30, 2013</b>
-		<br /><b>Austin, TX</b>
-		<br /><a href="http://www.elysumonline.net/" target="_parent">Elysium</a>
-		<br />705 Red River, Austin, TX
-		<br />with <a href="http://www.egolikeness.com/" target="_blank">Ego Likeness</a> (12am), <a href="https://www.facebook.com/pages/Servitor/344543773871" target="_blank">Servitor</a> (11pm) and <a href="https://www.facebook.com/XBRMNT" target="_blank">XBRMNT</a> (9pm)
-		<br />Doors open at 8 PM &bull; Show Starts at 8 PM &bull; $9 in advance &bull; $14 at the door &bull; 18+
-		<br /><a href="https://www.facebook.com/events/323911431084405/" target="_blank">View the Facebook Event</a>
-		<br />tMitG is scheduled for 10pm.</p>-->
+		<!--<div class="notopmargin" itemscope itemtype="https://schema.org/Event">
+		<b itemprop="startDate" value="2013-11-30">Saturday, November 30, 2013</b>
+		<br /><b itemprop="location">Austin, TX</b>
+		<br /><a href="http://www.elysumonline.net/" target="_parent" itemprop="name">Elysium</a>
+		<br /><span itemprop="streetAddress">705 Red River, Austin, TX</span>
+		<br />with <a href="http://www.egolikeness.com/" target="_blank" itemprop="performer">Ego Likeness</a> (12am), <a href="https://www.facebook.com/pages/Servitor/344543773871" target="_blank" itemprop="performer">Servitor</a> (11pm) and <a href="https://www.facebook.com/XBRMNT" target="_blank" itemprop="performer">XBRMNT</a> (9pm)
+		<br />Doors open at <time itemprop="doorTime">8 PM</time> &bull; Show Starts at 8 PM &bull; $9 in advance &bull; <span itemprop="price" content="14.00">$14</span> at the door &bull; 18+
+		<br /><a href="https://www.facebook.com/events/323911431084405/" target="_blank" itemprop="url">View the Facebook Event</a>
+		<br />tMitG is scheduled for 10pm.
+		</div>-->
 
 		<p class="notopmargin">No live shows are scheduled at this time.</p>
 

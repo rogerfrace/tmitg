de0ecd58b6d3419974249f3cc0d3558102254062
Roger Fracé <rogerfrace@gmail.com>
2026-04-11 18:11:02 -0500

    better news item validation

--- news.php
+++ news.php
@@ -1,6 +1,6 @@
 <?php
 //need to send different headers if this is a single-item view
-if (isset($_GET["item"])) {
+if (isset($_GET["item"]) && is_numeric($_GET["item"])) {
 	$titlenum = $_GET["item"];
 	if( (!preg_match('/^[0-9]{3}$/', $titlenum)) || (!file_exists("newsitems/".$titlenum.".php")) ) {
 		header('HTTP/1.0 404 not found');die('404 not found');

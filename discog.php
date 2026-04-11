19bc66f274ab49fd5ce5f692a42be8754994fc6b
Roger Fracé <rogerfrace@gmail.com>
2026-04-11 18:45:40 -0500

    lazy load compilation images

--- discog.php
+++ discog.php
@@ -61,67 +61,67 @@
 <section aria-labelledby="compilations">
 <h2 id="compilations">compilation appearances:</h2>
 <ul role="list" class="comps">
-<li><a href="unquietfinal.php" title="The Unquiet Grave: The Final Chapter (2022)"><img src="albums/unquietfinalico.jpg" width="75" height="75" alt="The Unquiet Grave: The Final Chapter"></a></li>
+<li><a href="unquietfinal.php" title="The Unquiet Grave: The Final Chapter (2022)"><img src="albums/unquietfinalico.jpg" width="75" height="75" alt="The Unquiet Grave: The Final Chapter" loading="lazy"></a></li>
 
-<li><a href="unearthd.php" title="Unearth'd (2021)"><img src="albums/unearthdico.jpg" width="75" height="75" alt="Unearth'd"></a></li>
+<li><a href="unearthd.php" title="Unearth'd (2021)"><img src="albums/unearthdico.jpg" width="75" height="75" alt="Unearth'd" loading="lazy"></a></li>
 
-<li><a href="ladansemacabre9.php" title="La Danse Macabre 9 (2020)"><img src="albums/ladansemacabre9ico.jpg" width="75" height="75" alt="La Danse Macabre 9"></a></li>
+<li><a href="ladansemacabre9.php" title="La Danse Macabre 9 (2020)"><img src="albums/ladansemacabre9ico.jpg" width="75" height="75" alt="La Danse Macabre 9" loading="lazy"></a></li>
 
-<li><a href="rebellion.php" title="Rebellion: Vol 1 (2015)"><img src="albums/rebellionico.png" width="75" height="75" alt="Rebellion: Vol 1"></a></li>
+<li><a href="rebellion.php" title="Rebellion: Vol 1 (2015)"><img src="albums/rebellionico.png" width="75" height="75" alt="Rebellion: Vol 1" loading="lazy"></a></li>
 
-<li><a href="drownedvisions.php" title="Drowned Visions (2011)"><img src="albums/drownedvisionsico.jpg" width="75" height="75" alt="Drowned Visions"></a></li>
+<li><a href="drownedvisions.php" title="Drowned Visions (2011)"><img src="albums/drownedvisionsico.jpg" width="75" height="75" alt="Drowned Visions" loading="lazy"></a></li>
 
-<li><a href="attrition.php" title="Wrapped in the Guise of My Friend (2009)"><img src="albums/attritionico.jpg" width="75" height="75" alt="Wrapped in the Guise of My Friend"></a></li>
+<li><a href="attrition.php" title="Wrapped in the Guise of My Friend (2009)"><img src="albums/attritionico.jpg" width="75" height="75" alt="Wrapped in the Guise of My Friend" loading="lazy"></a></li>
 
-<li><a href="unquietultimate.php" title="The Unquiet Grave - The Ultimate Goth Collection (2008)"><img src="albums/uqguico.jpg" width="75" height="75" alt="The Unquiet Grave - The Ultimate Goth Collection"></a></li>
+<li><a href="unquietultimate.php" title="The Unquiet Grave - The Ultimate Goth Collection (2008)"><img src="albums/uqguico.jpg" width="75" height="75" alt="The Unquiet Grave - The Ultimate Goth Collection" loading="lazy"></a></li>
 
-<li><a href="darkhorizons.php" title="Dark Horizons: Dance of the Dead (2008)"><img src="albums/darkhorizonsico.jpg" width="75" height="75" alt="Dark Horizons: Dance of the Dead"></a></li>
+<li><a href="darkhorizons.php" title="Dark Horizons: Dance of the Dead (2008)"><img src="albums/darkhorizonsico.jpg" width="75" height="75" alt="Dark Horizons: Dance of the Dead" loading="lazy"></a></li>
 
-<li><a href="c11.php" title="Convergence 11 CD Sampler"><img src="albums/c11ico.jpg" width="75" height="75" alt="Convergence 11 CD Sampler"></a></li>
+<li><a href="c11.php" title="Convergence 11 CD Sampler"><img src="albums/c11ico.jpg" width="75" height="75" alt="Convergence 11 CD Sampler" loading="lazy"></a></li>
 
-<li><a href="eclectica2.php" title="Eclectica 2 (2003)"><img src="albums/eclectica2ico.jpg" width="84" height="75" alt="Eclectica 2"></a></li>
+<li><a href="eclectica2.php" title="Eclectica 2 (2003)"><img src="albums/eclectica2ico.jpg" width="84" height="75" alt="Eclectica 2" loading="lazy"></a></li>
 
-<li><a href="bets.php" title="Cover Your Bets! (2003)"><img src="albums/betsico.jpg" width="75" height="75" alt="Cover Your Bets!"></a></li>
+<li><a href="bets.php" title="Cover Your Bets! (2003)"><img src="albums/betsico.jpg" width="75" height="75" alt="Cover Your Bets!" loading="lazy"></a></li>
 
-<li><a href="elegy.php" title="Elegy Sampler 25 (2002)"><img src="albums/elegyico.jpg" width="75" height="75" alt="Elegy Sampler 25"></a></li>
+<li><a href="elegy.php" title="Elegy Sampler 25 (2002)"><img src="albums/elegyico.jpg" width="75" height="75" alt="Elegy Sampler 25" loading="lazy"></a></li>
 
-<li><a href="PoW.php" title="Defend the Palace: Worms A.D.MCLXXXIV (2002)"><img src="albums/PoWico.jpg" width="75" height="75" alt="Defend the Palace: Worms A.D.MCLXXXIV"></a></li>
+<li><a href="PoW.php" title="Defend the Palace: Worms A.D.MCLXXXIV (2002)"><img src="albums/PoWico.jpg" width="75" height="75" alt="Defend the Palace: Worms A.D.MCLXXXIV" loading="lazy"></a></li>
 
-<li><a href="eclectica.php" title="Eclectica! (2001)"><img src="albums/eclecticaico.jpg" width="75" height="75" alt="Eclectica!"></a></li>
+<li><a href="eclectica.php" title="Eclectica! (2001)"><img src="albums/eclecticaico.jpg" width="75" height="75" alt="Eclectica!" loading="lazy"></a></li>
 
-<li><a href="misfit.php" title="A Misfit Xmas (2001)"><img src="albums/misfitico.jpg" width="75" height="75" alt="A Misfit Xmas"></a></li>
+<li><a href="misfit.php" title="A Misfit Xmas (2001)"><img src="albums/misfitico.jpg" width="75" height="75" alt="A Misfit Xmas" loading="lazy"></a></li>
 
-<li><a href="cantus.php" title="Cantus"><img src="albums/cantusico.jpg" width="75" height="75" alt="Cantus"></a></li>
+<li><a href="cantus.php" title="Cantus"><img src="albums/cantusico.jpg" width="75" height="75" alt="Cantus" loading="lazy"></a></li>
 
-<li><a href="tnog.php" title="The Nature of Gothic (2001)"><img src="albums/tnogico.jpg" width="75" height="75" alt="The Nature of Gothic"></a></li>
+<li><a href="tnog.php" title="The Nature of Gothic (2001)"><img src="albums/tnogico.jpg" width="75" height="75" alt="The Nature of Gothic" loading="lazy"></a></li>
 
-<li><a href="sde.php" title="Sex Death & Eyeliner (2001)"><img src="albums/sdeico.jpg" width="76" height="75" alt="Sex Death & Eyeliner (2001)"></a></li>
+<li><a href="sde.php" title="Sex Death & Eyeliner (2001)"><img src="albums/sdeico.jpg" width="76" height="75" alt="Sex Death & Eyeliner (2001)" loading="lazy"></a></li>
 
-<li><a href="tvccharity2.php" title="The Violet Collection 2"><img src="albums/tvccharity2ico.jpg" width="75" height="75" alt="The Violet Collection 2"></a></li>
+<li><a href="tvccharity2.php" title="The Violet Collection 2"><img src="albums/tvccharity2ico.jpg" width="75" height="75" alt="The Violet Collection 2" loading="lazy"></a></li>
 
-<li><a href="butoh.php" title="Butoh (2000)"><img src="albums/butohico.jpg" width="83" height="75" alt="Butoh"></a></li>
+<li><a href="butoh.php" title="Butoh (2000)"><img src="albums/butohico.jpg" width="83" height="75" alt="Butoh" loading="lazy"></a></li>
 
-<li><a href="legends.php" title="Immortal Legends (2000)"><img src="albums/legendsico.jpg" width="75" height="75" alt="Immortal Legends"></a></li>
+<li><a href="legends.php" title="Immortal Legends (2000)"><img src="albums/legendsico.jpg" width="75" height="75" alt="Immortal Legends" loading="lazy"></a></li>
 
-<li><a href="cocteau.php" title="Dark Treasures (2000)"><img src="albums/cocteauico.jpg" width="75" height="75" alt="Dark Treasures"></a></li>
+<li><a href="cocteau.php" title="Dark Treasures (2000)"><img src="albums/cocteauico.jpg" width="75" height="75" alt="Dark Treasures" loading="lazy"></a></li>
 
-<li><a href="tvccharity.php" title="The Violet Collection"><img src="albums/tvccharityico.jpg" width="75" height="75" alt="The Violet Collection"></a></li>
+<li><a href="tvccharity.php" title="The Violet Collection"><img src="albums/tvccharityico.jpg" width="75" height="75" alt="The Violet Collection" loading="lazy"></a></li>
 
-<li><a href="resistor.php" title="Resistor (2000)"><img src="albums/resistorico.jpg" width="75" height="75" alt="Resistor"></a></li>
+<li><a href="resistor.php" title="Resistor (2000)"><img src="albums/resistorico.jpg" width="75" height="75" alt="Resistor" loading="lazy"></a></li>
 
-<li><a href="excelsis.php" title="Excelsis Vol. 2 (1999)"><img src="albums/excelsisico.jpg" width="75" height="75" alt="Excelsis Vol. 2"></a></li>
+<li><a href="excelsis.php" title="Excelsis Vol. 2 (1999)"><img src="albums/excelsisico.jpg" width="75" height="75" alt="Excelsis Vol. 2" loading="lazy"></a></li>
 
-<li><a href="unquiet.php" title="The Unquiet Grave Volume One (1999)"><img src="albums/uqgico.jpg" width="75" height="75" alt="The Unquiet Grave Volume One"></a></li>
+<li><a href="unquiet.php" title="The Unquiet Grave Volume One (1999)"><img src="albums/uqgico.jpg" width="75" height="75" alt="The Unquiet Grave Volume One" loading="lazy"></a></li>
 
-<li><a href="eternal.php" title="What Is Eternal (1998)"><img src="albums/eternalico.jpg" width="87" height="75" alt="What Is Eternal"></a></li>
+<li><a href="eternal.php" title="What Is Eternal (1998)"><img src="albums/eternalico.jpg" width="87" height="75" alt="What Is Eternal" loading="lazy"></a></li>
 
-<li><a href="arofv3.php" title="Abort Retry or Fail Vol. 3 (1998)"><img src="albums/arofv3ico.jpg" width="77" height="75" alt="Abort Retry or Fail Vol. 3"></a></li>
+<li><a href="arofv3.php" title="Abort Retry or Fail Vol. 3 (1998)"><img src="albums/arofv3ico.jpg" width="77" height="75" alt="Abort Retry or Fail Vol. 3" loading="lazy"></a></li>
 
-<li><a href="sogrey.php" title="Shades of Grey (1998)"><img src="albums/sogreyico.jpg" width="77" height="75" alt="Shades of Grey"></a></li>
+<li><a href="sogrey.php" title="Shades of Grey (1998)"><img src="albums/sogreyico.jpg" width="77" height="75" alt="Shades of Grey" loading="lazy"></a></li>
 
-<li><a href="hexfiles.php" title="Hex Files, Vol. 2 (1997)"><img src="albums/hexfilesico.jpg" width="84" height="75" alt="Hex Files, Vol. 2"></a></li>
+<li><a href="hexfiles.php" title="Hex Files, Vol. 2 (1997)"><img src="albums/hexfilesico.jpg" width="84" height="75" alt="Hex Files, Vol. 2" loading="lazy"></a></li>
 
-<li><a href="mindbody.php" title="Mind/Body³ (1995)"><img src="albums/mindbodyico.jpg" width="75" height="75" alt="Mind/Body2"></a></li>
+<li><a href="mindbody.php" title="Mind/Body³ (1995)"><img src="albums/mindbodyico.jpg" width="75" height="75" alt="Mind/Body2" loading="lazy"></a></li>
 </ul>
 </section>
 

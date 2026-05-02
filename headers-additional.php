c55702023a0d94af0445df5b755e87f8a26b76a4
Roger Fracé <rogerfrace@gmail.com>
2026-05-02 10:36:14 -0500

    web fonts acknowledge OS font scaling pref

--- headers-additional.php
+++ headers-additional.php
@@ -1,4 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="text-scale" content="scale">
 <meta property="fb:app_id" content="121619441387">
 <meta property="fb:admins" content="1120445561,713632115">
 <link rel="preconnect" href="https://fonts.googleapis.com">

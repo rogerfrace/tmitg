3d765b2a842a956a826edc9b37503125d159f8e7
Roger Fracé <rogerfrace@gmail.com>
2026-08-12 09:33:47 -0500

    js load tweak

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ require_once "functions.php";
 	<link rel="stylesheet" type="text/css" href="tmitg.css" />
 	<?php include_once "fontawesome.inc.php"; ?>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-	<script src="tmitg.js"></script>
+	<script src="tmitg.js" defer></script>
 	<script type="application/ld+json">
 	{
 		"@context": "https://schema.org",

cb24cbc06bc922682cf6a67496c145e6d114a434
Roger Fracé <rogerfrace@gmail.com>
2026-08-14 09:30:15 -0500

    update location of llms.txt

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ require_once "functions.php";
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preload" as="style" href="tmitg-mobile.css">
 	<link rel="stylesheet" type="text/css" href="tmitg.css" />
-	<link rel="describedby" href="/llms.txt" />
+	<link rel="describedby" href="/.well-known/llms.txt" />
 	<?php include_once "fontawesome.inc.php"; ?>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="tmitg.js" defer></script>

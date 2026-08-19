be84c317b22a0a253361a64ad275cd9171039ec4
Roger Fracé <rogerfrace@gmail.com>
2026-08-19 13:25:54 -0500

    fixing the link to index.md again

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ require_once "functions.php";
 	<link rel="preload" as="style" href="tmitg-mobile.css">
 	<link rel="stylesheet" type="text/css" href="tmitg.css" />
 	<link rel="describedby" href="/llms.txt" />
-	<link rel="alternate" type="text/markdown" href="/index.md.php" />
+	<link rel="alternate" type="text/markdown" href="/index.md" />
 	<?php include_once "fontawesome.inc.php"; ?>
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="tmitg.js" defer></script>

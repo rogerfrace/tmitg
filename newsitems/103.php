480e2904694c77e091b58672a87a9b1bb1f36c8b
Roger Fracé <rogerfrace@gmail.com>
2026-09-02 17:14:16 -0500

    Space-Time streaming newsitem

---
+++ newsitems/103.php
@@ -0,0 +1,11 @@
+<?php
+
+$pubdate = "Wed, 02 Sep 2026 17:00:00 CDT";
+$title = "Space-Time now available for streaming";
+$description = "<a href=\"https://tmitg.bandcamp.com/track/space-time\"><img itemprop=\"image\" src=\"https://www.tmitg.com/albums/spacetime.jpg\" alt=\"Space-Time\" /></a><p>We've released our latest single \"Space-Time\" from May to streaming services, so it's now available for your listening enjoyment on Spotify, Apple Music, Amazon MP3, and other platforms.</p>
+<p><a href=\"https://open.spotify.com/track/3EBTarIggE3KecGzghAx8g?si=a5331bf67c1c41f3\">Spotify</p>
+<p><a href=\"https://geo.music.apple.com/us/album/space-time-single/6807489729?itsct=music_box&itscg=30200&at=1000l35Bw&app=music&ls=1\">Apple Music</p>
+<p><a href=\"https://www.amazon.com/music/player/albums/B0HHG4PKFP?&linkCode=ll2&tag=themachininth-20&linkId=2b38dd310e90ee1379789ce27132a9ae&language=en_US&ref_=as_li_ss_tl\">Amazon MP3</p>
+";
+
+?>

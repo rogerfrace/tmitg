a97aa4aa816a4eda52301d30b71ae38ad123af34
Roger Fracé <rogerfrace@gmail.com>
2026-09-21 20:54:38 -0500

    104 url fix

--- newsitems/104.php
+++ newsitems/104.php
@@ -2,7 +2,7 @@
 
 $pubdate = "Fri, 02 Oct 2026 08:00:00 CDT";
 $title = "New photoshoot";
-$description = "<img itemprop=\"image\" src=\"http://192.168.86.26:8000/photos/2026-IMG_7880-ico.jpg\" alt=\"Roger and Summer of the Machine in the Garden\" /> We took new photos last month! Now available to view in our <a href=\"https://www.tmitg.com/images.php\">images gallery</a>.
+$description = "<img itemprop=\"image\" src=\"https://www.tmitg.com/photos/2026-IMG_7880-ico.jpg\" alt=\"Roger and Summer of the Machine in the Garden\" /> We took new photos last month! Now available to view in our <a href=\"https://www.tmitg.com/images.php\">images gallery</a>.
 ";
 
 ?>

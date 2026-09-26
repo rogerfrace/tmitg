74ca25f26a4533f54fc01ce4799ee73375a951b9
Roger Fracé <rogerfrace@gmail.com>
2026-09-26 14:53:43 -0500

    big news release

---
+++ newsitems/105.php
@@ -0,0 +1,24 @@
+<?php
+
+$pubdate = "Mon, 05 Oct 2026 08:00:00 CDT";
+$title = "New EP Announcement: Impermanence";
+$description = "<img itemprop=\"image\" src=\"https://www.tmitg.com/albums/impermanence.jpg\" alt=\"Impermanence\" />
+
+<p>the Machine in the Garden is proud to announce the forthcoming release of ‘Impermanence,’ their tenth studio release and second EP in their catalog, coming October 30th.</p>
+
+<p>Six songs exploring impermanence, fate, loss, and the existential work of dealing with circumstances beyond one’s control, 'Impermanence' is tMitG's first major release in six years. Tighter in focus and stripped down to just the machine and voice, it's still as rich, engaging, and emotional as anything that's come before it.</p>
+
+<p>tMitG previewed 'Impermanence' with the single 'Space-Time' in May, to be followed by a second single and video for 'The Beginning of All Things' this Friday, October 9th.</p>
+
+<p>Consistent with the two singles, the cover art for ‘Impermanence’ features an image from NASA of the Lion Nebula, itself a fitting image for the title. NASA writes: “The nebula...has formed and continues to be sculpted by the remains of a dying star. The Lion Nebula is a snapshot in time.”</p>
+
+<p>“I wanted to get back to some basics with the music, focusing mainly on analog synth sounds,” explains Roger. “The retro-80s style has been a new modern thing for 20 years, but I wanted to explore it on my own terms. Even with the synthetic sounds, I feel like we still created something organic and emotional, effectively combining what we do best in composition and production with retro sounds and styles. I even got to pull in some classic Roland and other samples I’ve had sitting on my drive since the mid-90s, which was fun too.”</p>
+
+<p>Summer adds “While I hesitate to use the words ‘existential crisis’ there has been a large amount of personal hardship these past few years. I’ve overcome a lot of death and loss as well as faced my own mortality so there was a lot of processing to do. I wanted to explore themes around our place in the universe, why things unfold the way they do, and the fleeting nature of our existence. It may sound bleak, but I hope listeners hear hope on the other side of great tragedy.”</p>
+
+<p>‘Impermanence’ will be released on October 30th, 2026, available as a digital download from <a href=\"https://tmitg.bandcamp.com\">tMitG’s Bandcamp shop</a> and as on-demand physical media from <a href=\"https://elasticstage.com/tmitg\">tMitG’s ElasticStage shop</a> as a CD and, for the first time ever, vinyl.</p>
+
+<p>‘Impermanence’ will also be available on release day to stream from Spotify, Apple Music, Amazon Music, and other streaming services. More information on 'Impermanence' and “The Beginning on All Things” can be found at <a href=\"https://www.tmitg.com\">www.tmitg.com</a>.</p>
+";
+
+?>
